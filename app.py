56788fec6579dfcfdbde17e2f77ed48ccf4c0415
123ManvendraSingh <143689929+123ManvendraSingh@users.noreply.github.com>
2024-03-11 23:11:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import nltk
 nltk.download('universal_tagset')
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from collections import Counter
